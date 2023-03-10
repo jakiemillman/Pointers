5e5e86fd7c39ca80a2bedc3749c8204fe0d36ba0
777 <777@DESKTOP-T0PRUM1>
2023-03-10 22:00:56 +0800

    Split

---
+++ Pointers/Split.cpp
@@ -0,0 +1,65 @@
+//Split.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int const size = 10;
+	int array[size];
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rand() % 100;
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
